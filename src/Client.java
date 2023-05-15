c8d9cbbaac2f38ebef2985b09762e0960e525540
Sandeep mainali <amanshrestha284@gmail.com>
2023-05-15 11:25:43 +0545

    socket/Tcp

---
+++ src/Client.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static  void main (String[] args){
+
+        try {
+            Socket socket = new Socket("localhost",12345);
+            //asking user input with buffer
+            BufferedReader userInputBuffer = new BufferedReader(new InputStreamReader(System.in));
+
+            //taking data from socket buffer
+            BufferedReader socketBuffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //to send the message
+            OutputStream outputStream =  socket.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            String sendingMessage ,receivingMessage;
+            while(true){
+
+                receivingMessage = socketBuffer.readLine();
+                System.out.println("Message from client: " +receivingMessage);
+                sendingMessage = userInputBuffer.readLine();
+                printWriter.println(sendingMessage);
+                printWriter.flush();
+
+
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
